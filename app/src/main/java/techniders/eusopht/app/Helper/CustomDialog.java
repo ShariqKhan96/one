a62c24f9b2e033a9a11e45fb3863f20c126bb4fd
ShariqKhan96 <30958488+ShariqKhan96@users.noreply.github.com>
2018-01-25 13:34:55 -0700

    Initial commit

---
+++ app/src/main/java/techniders/eusopht/app/Helper/CustomDialog.java
@@ -0,0 +1,19 @@
+package techniders.eusopht.app.Helper;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.view.Window;
+
+import techniders.eusopht.app.R;
+
+
+public class CustomDialog extends ProgressDialog {
+
+    public CustomDialog(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setIndeterminate(true);
+        setMessage(context.getResources().getString(R.string.please_wait));
+      //  setContentView(R.layout.custom_dialog);
+    }
+}
